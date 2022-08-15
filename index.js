5e8bb7b59122ac3b77671c7571d9224c7a05f1bf
dotoracle <dotoracle.network@gmail.com>
2022-08-15 17:59:39 +0700

    update js

--- index.js
+++ index.js
@@ -4,8 +4,51 @@ const {
   CasperContractClient,
 } = require("casper-js-client-helper");
 
-const { setClient, contractSimpleGetter } = helpers;
+const CEP78 = require("casper-cep78js");
+const {
+  CLValueBuilder,
+  CasperClient,
+  CLByteArray,
+  RuntimeArgs,
+} = require("casper-js-sdk");
+const { DEFAULT_TTL } = require("casper-js-client-helper/dist/constants");
 
+const { setClient, contractSimpleGetter, createRecipientAddress } = helpers;
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const getDeploy = async (NODE_URL, deployHash) => {
+  const client = new CasperClient(NODE_URL);
+  let i = 300;
+  while (i != 0) {
+    const [deploy, raw] = await client.getDeploy(deployHash);
+    if (raw.execution_results.length !== 0) {
+      // @ts-ignore
+      if (raw.execution_results[0].result.Success) {
+        return deploy;
+      } else {
+        // @ts-ignore
+        throw Error(
+          "Contract execution: " +
+            // @ts-ignore
+            raw.execution_results[0].result.Failure.error_message
+        );
+      }
+    } else {
+      i--;
+      await sleep(1000);
+      continue;
+    }
+  }
+  throw Error("Timeout after " + i + "s. Something's wrong");
+};
+
+const genRanHex = (size = 64) =>
+  [...Array(size)]
+    .map(() => Math.floor(Math.random() * 16).toString(16))
+    .join("");
 const NFTBridge = class {
   constructor(contractHash, nodeAddress, chainName) {
     this.contractHash = contractHash.startsWith("hash-")
@@ -16,12 +59,20 @@ const NFTBridge = class {
     this.contractClient = new CasperContractClient(nodeAddress, chainName);
   }
 
+  static async createInstance(contractHash, nodeAddress, chainName) {
+    let bridge = new NFTBridge(contractHash, nodeAddress, chainName);
+    await bridge.init();
+    return bridge;
+  }
+
   async init() {
+    console.log("intializing", this.nodeAddress, this.contractHash);
     const { contractPackageHash, namedKeys } = await setClient(
       this.nodeAddress,
       this.contractHash,
       ["request_ids"]
     );
+    console.log("done");
     this.contractPackageHash = contractPackageHash;
     this.contractClient.chainName = this.chainName;
     this.contractClient.contractHash = this.contractHash;
@@ -51,11 +102,91 @@ const NFTBridge = class {
         itemKey,
         this.namedKeys.requestIds
       );
-      return result.val.data.data.toString();
+      return result;
     } catch (e) {
       throw e;
     }
   }
+  async requestBridgeNFT({
+    keys,
+    tokenIds,
+    nftContractHash,
+    toChainId,
+    identifierMode,
+    paymentAmount,
+    ttl,
+    receiverAddress
+  }) {
+    if (!paymentAmount) {
+      paymentAmount = paymentAmount ? paymentAmount : "5000000000";
+      ttl = ttl ? ttl : DEFAULT_TTL;
+    }
+
+    if (identifierMode == undefined) {
+      let nftContract = new CEP78(
+        nftContractHash,
+        this.nodeAddress,
+        this.chainName
+      );
+      await nftContract.init();
+      identifierMode = await nftContract.identifierMode();
+    }
+    nftContractHash = nftContractHash.startsWith("hash-")
+      ? nftContractHash.slice(5)
+      : nftContractHash;
+    console.log("nftContractHash", nftContractHash);
+    nftContractHash = new CLByteArray(
+      Uint8Array.from(Buffer.from(nftContractHash, "hex"))
+    );
+    let runtimeArgs = {};
+    if (identifierMode == 0) {
+      tokenIds = tokenIds.map((e) => CLValueBuilder.u64(e));
+      runtimeArgs = RuntimeArgs.fromMap({
+        token_ids: CLValueBuilder.list(tokenIds),
+        identifier_mode: CLValueBuilder.u8(identifierMode),
+        nft_contract_hash: createRecipientAddress(nftContractHash),
+        to_chainid: CLValueBuilder.u256(toChainId),
+        request_id: CLValueBuilder.string(genRanHex()),
+        receiver_address: CLValueBuilder.string(receiverAddress)
+      });
+    } else {
+      tokenIds = tokenIds.map((e) => CLValueBuilder.string(e));
+      runtimeArgs = RuntimeArgs.fromMap({
+        token_hashes: tokenIds,
+        identifier_mode: CLValueBuilder.u8(identifierMode),
+        nft_contract_hash: createRecipientAddress(nftContractHash),
+        to_chainid: CLValueBuilder.u256(toChainId),
+        request_id: CLValueBuilder.string(genRanHex()),
+        receiver_address: CLValueBuilder.string(receiverAddress)
+      });
+    }
+
+    console.log("sending");
+    let trial = 5;
+    while (true) {
+      try {
+        let hash = await this.contractClient.contractCall({
+          entryPoint: "request_bridge_nft",
+          keys: keys,
+          paymentAmount,
+          runtimeArgs,
+          cb: (deployHash) => {
+            console.log("deployHash", deployHash);
+          },
+          ttl,
+        });
+
+        return hash;
+      } catch (e) {
+        trial--
+        if (trial == 0) {
+          throw e;
+        }
+        console.log('waiting 2 seconds')
+        await sleep(3000)
+      }
+    }
+  }
 };
 
-module.exports = { NFTBridge };
+module.exports = { NFTBridge, genRanHex };
